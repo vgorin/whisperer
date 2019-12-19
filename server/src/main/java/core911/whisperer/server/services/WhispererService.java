40f25519819aec70b41295c997498300ab7edc60
vgorin <vgrn.at@gmail.com>
2019-12-19 06:23:05 +0200

    add MessageEnvelope validation

--- server/src/main/java/core911/whisperer/server/services/WhispererService.java
+++ server/src/main/java/core911/whisperer/server/services/WhispererService.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.time.Instant;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -35,6 +37,7 @@ public class WhispererService {
     @Path("/{topic}")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public void putByTopic(@PathParam("topic") int topic, List<MessageEnvelope> envelopes) {
+        validateEnvelopes(envelopes);
         throw new ServerErrorException(501);
     }
 
@@ -42,7 +45,50 @@ public class WhispererService {
     @Path("/")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public void putMessages(List<MessageEnvelope> envelopes) {
+        validateEnvelopes(envelopes);
         throw new ServerErrorException(501);
+    }
+
+    private void validateEnvelope(MessageEnvelope envelope) {
+        if(envelope == null) {
+            log.trace("No envelope: {}", envelope);
+            throw new BadRequestException("No envelope");
+        }
+        if(envelope.expiry <= Instant.now().getEpochSecond()) {
+            log.trace("Expired envelope: {}", envelope);
+            throw new BadRequestException("Already expired!");
+        }
+        if(envelope.ttl < 0) {
+            log.trace("Invalid TTL: {}", envelope);
+            throw new BadRequestException("Invalid TTL!");
+        }
+        if(envelope.expiry - envelope.ttl > Instant.now().getEpochSecond()) {
+            log.trace("Invalid implied insertion time: {}", envelope);
+            throw new BadRequestException("Invalid implied insertion time! You will be punished!!!");
+        }
+        // TODO: implement real nonce validation
+        if(envelope.nonce != 3141592653589793L) {
+            log.trace("Invalid nonce: {}", envelope);
+            throw new BadRequestException("Invalid nonce! Work more to find the correct nonce.");
+        }
+        if(envelope.message == null) {
+            log.trace("Empty message: {}", envelope);
+            throw new BadRequestException("Empty message!");
+        }
+        if(envelope.message.length != 0x100) {
+            log.trace("Invalid message size ({} bytes): {}", envelope.message.length, envelope);
+            throw new BadRequestException("Invalid message size!");
+        }
+    }
+
+    private void validateEnvelopes(Collection<MessageEnvelope> envelopes) {
+        if(envelopes == null || envelopes.isEmpty()) {
+            log.trace("Empty request: no envelopes: {}", envelopes);
+            throw new BadRequestException("Empty request!");
+        }
+        for(MessageEnvelope envelope: envelopes) {
+            validateEnvelope(envelope);
+        }
     }
 
 }
